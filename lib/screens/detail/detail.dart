8ceb14ad030ee6f1d016a97ebc0e66374072c04c
Hyejin1010 <hyepago@naver.com>
2021-06-27 15:44:33 +0900

    ✨ Feat: Detail 페이지 간 이동 및 애니메이션

--- lib/screens/detail/detail.dart
+++ lib/screens/detail/detail.dart
@@ -12,6 +12,25 @@ class DetailScreen extends StatefulWidget {
   _DetailScreenState createState() => _DetailScreenState();
 }
 
+const Map<String, dynamic> ARROW_DATA = {
+  'top': {
+    'begin': Offset(0.0, -1.0),
+    'index': -3,
+  },
+  'right': {
+    'begin': Offset(1.0, 0.0),
+    'index': 1,
+  },
+  'bottom': {
+    'begin': Offset(0.0, 1.0),
+    'index': 3,
+  },
+  'left': {
+    'begin': Offset(-1.0, 0.0),
+    'index': -1,
+  },
+};
+
 class _DetailScreenState extends State<DetailScreen> {
   late Size size;
 
@@ -23,7 +42,7 @@ class _DetailScreenState extends State<DetailScreen> {
 
   @override
   Widget build(BuildContext context) {
-    final double mandalSize = Functions.getMandalSize(size);
+    final double mandalSize = Functions.getMandalSize(size) - 100;
 
     return Scaffold(
       body: SafeArea(
@@ -39,20 +58,78 @@ class _DetailScreenState extends State<DetailScreen> {
               ),
             ),
             Expanded(
-              child: Center(
-                child: SizedBox(
-                  width: mandalSize,
-                  height: mandalSize,
-                  child: Hero(
-                    tag: 'mandal-item-${widget.index}',
-                    child: Item(),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  if (widget.index > 2)
+                    _buildArrowIconButton('top'),
+                  Center(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        widget.index % 3 != 0
+                          ? _buildArrowIconButton('left')
+                          : SizedBox(width: 40.0),
+                        SizedBox(
+                          width: mandalSize,
+                          height: mandalSize,
+                          child: Hero(
+                            tag: 'mandal-item-${widget.index}',
+                            child: Item(),
+                          ),
+                        ),
+                        widget.index % 3 != 2
+                          ? _buildArrowIconButton('right')
+                          : SizedBox(width: 40.0),
+                      ],
+                    ),
                   ),
-                ),
+                  if (widget.index < 6)
+                    _buildArrowIconButton('bottom'),
+                ],
               ),
             ),
           ],
         ),
       ),
     );
+  }
+
+  Widget _buildArrowIconButton(String arrow) {
+    IconData iconData = Icons.arrow_forward;
+    switch (arrow) {
+      case 'top':
+        iconData = Icons.arrow_upward;
+        break;
+      case 'left':
+        iconData = Icons.arrow_back;
+        break;
+      case 'bottom':
+        iconData = Icons.arrow_downward;
+        break;
+    }
+
+    return IconButton(
+      onPressed: () => _clickArrow(arrow),
+      icon: Icon(iconData),
+    );
+  }
+
+  void _clickArrow (String arrow) {
+    int arrowNum = ARROW_DATA[arrow]['index'] ?? 0;
+    int index = widget.index + arrowNum;
+    Navigator.of(context).pushReplacement(PageRouteBuilder(
+      pageBuilder: (_, __, ___) => DetailScreen(index: index),
+      transitionsBuilder: (context, animation, secondaryAnimation, child) {
+        const Offset end = Offset.zero;
+        Offset begin = ARROW_DATA[arrow]?['begin'] ?? Offset.zero;
+        var curve = Curves.ease;
+        var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+        return SlideTransition(
+          position: animation.drive(tween),
+          child: child,
+        );
+      },
+    ));
   }
 }
