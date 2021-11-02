ff39da2e646064590d3d02c8c0bcddd5276ea442
hyejin-1010 <hyepago@gmail.com>
2021-11-02 16:18:04 +0900

    Mod: MandalArt data 변경

--- lib/controllers/data_controller.dart
+++ lib/controllers/data_controller.dart
@@ -7,6 +7,17 @@ class DataController extends GetxController {
   Database? database;
   RxMap<int, Map<int, MandalArtModel>> data = RxMap<int, Map<int, MandalArtModel>>();
 
+  void initialize() async {
+    data = RxMap<int, Map<int, MandalArtModel>>();
+
+    try {
+      await _openDatabase();
+      await _getMandalArtData();
+    } catch (err) { throw err; }
+
+    update();
+  }
+
   Future<bool> updateItem(int group, int index, String content) async {
     MandalArtModel? item = data[group]?[index];
     if (item == null || database == null) { return false; }
@@ -21,24 +32,13 @@ class DataController extends GetxController {
     } catch (_) { return false; }
   }
 
-  void initialize() async {
-    data = RxMap<int, Map<int, MandalArtModel>>();
-
-    try {
-      await _openDatabase();
-      await _getMandalArtData();
-    } catch (err) { throw err; }
-
-    update();
-  }
-
   Future<void> _openDatabase() async {
     var databasesPath = await getDatabasesPath();
     String path = join(databasesPath, 'mandalart.db');
     database = await openDatabase(path, version: 1,
       onCreate: (Database db, int version) async {
         await db.execute(
-          'CREATE TABLE MandalArt (id INTEGER PRIMARY KEY, parent INTEGER, no INTERGER, content TEXT)'
+          'CREATE TABLE MandalArt (id INTEGER PRIMARY KEY, parent INTEGER, no INTEGER, content TEXT)'
         );
       });
     // deleteDatabase(path);
@@ -48,28 +48,45 @@ class DataController extends GetxController {
     if (database == null) { return; }
     final List<Map<String, dynamic>> maps = await database!.query('MandalArt');
     if (maps.length == 0) { return _initTable(); }
-    for (Map<String, dynamic> json in maps) {
+    if (data[4] == null) { data[4] = {}; }
+      for (Map<String, dynamic> json in maps) {
       MandalArtModel item = MandalArtModel.fromJson(json);
       if (data[item.group] == null) { data[item.group] = {}; }
       data[item.group]![item.index] = item;
+      if (item.index == 4) { data[4]![item.group] = item; }
     }
+  }
+
+  Future<MandalArtModel> _insert(int group, int index) async {
+    int id = (group * 10) + index + 1;
+    MandalArtModel item = MandalArtModel(id: id, group: group, index: index);
+    await database!.insert(
+      'MandalArt',
+      item.toMap(),
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+    data[group]![index] = item;
+    if (index == 4) {
+      if (data[4] == null) { data[4] = {}; }
+      data[4]![group] = item;
+    }
+    return item;
   }
 
   void _initTable() async {
     if (database == null) { return; }
 
     for (int group = 0; group < 9; group ++) {
+      if (group == 4) {
+        try {
+          _insert(group, 4);
+        } catch (error) { throw error; }
+        continue;
+      }
       data[group] = {};
       for (int index = 0; index < 9; index ++) {
         try {
-          int id = (group * 10) + index + 1;
-          MandalArtModel item = MandalArtModel(id: id, group: group, index: index);
-          await database!.insert(
-            'MandalArt',
-            item.toMap(),
-            conflictAlgorithm: ConflictAlgorithm.replace,
-          );
-          data[group]![index] = item;
+          await _insert(group, index);
         } catch (error) { throw error; }
       }
     }
